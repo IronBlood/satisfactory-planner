338e59738a93fc0b873bc2c7f0fa967fc7813ced
IronBlood <aplshenyang@gmail.com>
2026-02-25 14:16:48 +0800

    Cardview

---
+++ src/Cardview.tsx
@@ -0,0 +1,21 @@
+type CardviewProps = {
+  name: string;
+  image: string;
+};
+
+export default function Cardview({ name, image }: CardviewProps) {
+  return (
+    <div className="cursor-pointer rounded-lg bg-slate-700 p-2 hover:bg-slate-800 transition duration-200 ease-in-out">
+      <img
+        alt={name}
+        loading="lazy"
+        width="256"
+        height="256"
+        decoding="async"
+        className="m-auto aspect-square w-full max-w-32"
+        src={image}
+      />
+      <div className="m-2 text-center text-sm">{name}</div>
+    </div>
+  );
+}
