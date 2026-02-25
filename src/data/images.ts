f337a331c56b70963fab62b061a49af361e54334
IronBlood <aplshenyang@gmail.com>
2026-02-25 11:59:25 +0800

    image loader

---
+++ src/data/images.ts
@@ -0,0 +1,14 @@
+const images = import.meta.glob("../assets/*.png", {
+  eager: true,
+  import: "default",
+}) as Record<string, string>;
+
+const imageByName: Record<string, string> = {};
+for (const path in images) {
+  const fileName = path.split("/").pop()!;
+  imageByName[fileName] = images[path];
+}
+
+export {
+  imageByName,
+};
