ca4114e6be2e513f438da9ac66e7f7782103e4aa
IronBlood <aplshenyang@gmail.com>
2026-02-26 11:36:42 +0800

    miners

--- src/data/buildings.ts
+++ src/data/buildings.ts
@@ -2,8 +2,8 @@ import { imageByName } from "./images";
 
 export const BuildingNames = {
   MinerMk1: "Miner Mk.1",
-  // MinerMk2: "Miner Mk.2",
-  // MinerMk3: "Miner Mk.3",
+  MinerMk2: "Miner Mk.2",
+  MinerMk3: "Miner Mk.3",
 } as const;
 
 export type BuildingName = typeof BuildingNames[keyof typeof BuildingNames];
@@ -17,7 +17,17 @@ export interface Building {
 export const Buildings: Record<BuildingName, Building> = {
   [BuildingNames.MinerMk1]: {
     name: BuildingNames.MinerMk1,
-    image: imageByName["MinerMk1_256.png"],
+    image: imageByName["IconDesc_MinerMk1_256.png"],
     power: 5,
+  },
+  [BuildingNames.MinerMk2]: {
+    name: BuildingNames.MinerMk2,
+    image: imageByName["IconDesc_MinerMk2_256.png"],
+    power: 15,
+  },
+  [BuildingNames.MinerMk3]: {
+    name: BuildingNames.MinerMk3,
+    image: imageByName["IconDesc_MinerMk3_256.png"],
+    power: 45,
   },
 };
