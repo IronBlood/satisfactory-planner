76899ee4c37becc724f81d5c75fe306b2541d500
IronBlood <aplshenyang@gmail.com>
2026-02-26 15:18:56 +0800

    data: ore recipes

--- src/data/recipes.ts
+++ src/data/recipes.ts
@@ -1,5 +1,21 @@
 import {
   OreItemNames,
+  IngotItemNames,
+  MineralItemNames,
+  LiquidItemNames,
+  GasItemNames,
+  StandardPartItemNames,
+  IndustrialPartItemNames,
+  ElectronicItemNames,
+  CommunicationItemNames,
+  QuantumTechnologyItemNames,
+  ContainerItemNames,
+  FuelItemNames,
+  ConsumedItemNames,
+  AmmoItemNames,
+  NuclearItemNames,
+  WasteItemNames,
+  SpecialItemNames,
   OrePurities,
   type ItemName,
   type OrePurity,
@@ -49,6 +65,306 @@ export const Recipes: Recipe[] = [
       }).flat();
     }).flat();
   }).flat()),
+  {
+    name: "Limestone (Sulfur)",
+    inputs: [
+      {
+        name: ElectronicItemNames.ReanimatedSAM,
+        rate: 10,
+      },
+      {
+        name: OreItemNames.Sulfur,
+        rate: 20,
+      },
+    ],
+    outputs: [
+      {
+        name: OreItemNames.Limestone,
+        rate: 120,
+      },
+    ],
+    building: BuildingNames.Converter,
+  },
+  {
+    name: "Iron Ore (Limestone)",
+    inputs: [
+      {
+        name: ElectronicItemNames.ReanimatedSAM,
+        rate: 10,
+      },
+      {
+        name: OreItemNames.Limestone,
+        rate: 240,
+      },
+    ],
+    outputs: [
+      {
+        name: OreItemNames.IronOre,
+        rate: 120,
+      },
+    ],
+    building: BuildingNames.Converter,
+  },
+  {
+    name: "Copper Ore (Quartz)",
+    inputs: [
+      {
+        name: ElectronicItemNames.ReanimatedSAM,
+        rate: 10,
+      },
+      {
+        name: OreItemNames.RawQuartz,
+        rate: 100,
+      },
+    ],
+    outputs: [
+      {
+        name: OreItemNames.CopperOre,
+        rate: 120,
+      },
+    ],
+    building: BuildingNames.Converter,
+  },
+  {
+    name: "Copper Ore (Sulfur)",
+    inputs: [
+      {
+        name: ElectronicItemNames.ReanimatedSAM,
+        rate: 10,
+      },
+      {
+        name: OreItemNames.Sulfur,
+        rate: 120,
+      },
+    ],
+    outputs: [
+      {
+        name: OreItemNames.CopperOre,
+        rate: 120,
+      },
+    ],
+    building: BuildingNames.Converter,
+  },
+  {
+    name: "Caterium Ore (Copper)",
+    inputs: [
+      {
+        name: ElectronicItemNames.ReanimatedSAM,
+        rate: 10,
+      },
+      {
+        name: OreItemNames.CopperOre,
+        rate: 150,
+      },
+    ],
+    outputs: [
+      {
+        name: OreItemNames.CateriumOre,
+        rate: 120,
+      }
+    ],
+    building: BuildingNames.Converter,
+  },
+  {
+    name: "Caterium Ore (Quartz)",
+    inputs: [
+      {
+        name: ElectronicItemNames.ReanimatedSAM,
+        rate: 10,
+      },
+      {
+        name: OreItemNames.RawQuartz,
+        rate: 120
+      },
+    ],
+    outputs: [
+      {
+        name: OreItemNames.CateriumOre,
+        rate: 120,
+      }
+    ],
+    building: BuildingNames.Converter,
+  },
+  {
+    name: "Coal (Iron)",
+    building: BuildingNames.Converter,
+    inputs: [
+      {
+        name: ElectronicItemNames.ReanimatedSAM,
+        rate: 10,
+      },
+      {
+        name: OreItemNames.IronOre,
+        rate: 180,
+      },
+    ],
+    outputs: [
+      {
+        name: OreItemNames.Coal,
+        rate: 120,
+      },
+    ],
+  },
+  {
+    name: "Coal (Limestone)",
+    building: BuildingNames.Converter,
+    inputs: [
+      {
+        name: ElectronicItemNames.ReanimatedSAM,
+        rate: 10,
+      },
+      {
+        name: OreItemNames.Limestone,
+        rate: 360,
+      },
+    ],
+    outputs: [
+      {
+        name: OreItemNames.Coal,
+        rate: 120,
+      },
+    ],
+  },
+  {
+    name: "Raw Quartz (Bauxite)",
+    building: BuildingNames.Converter,
+    inputs: [
+      {
+        name: ElectronicItemNames.ReanimatedSAM,
+        rate: 10,
+      },
+      {
+        name: OreItemNames.Bauxite,
+        rate: 100,
+      },
+    ],
+    outputs: [
+      {
+        name: OreItemNames.RawQuartz,
+        rate: 120,
+      },
+    ],
+  },
+  {
+    name: "Raw Quartz (Coal)",
+    building: BuildingNames.Converter,
+    inputs: [
+      {
+        name: ElectronicItemNames.ReanimatedSAM,
+        rate: 10,
+      },
+      {
+        name: OreItemNames.Coal,
+        rate: 240,
+      },
+    ],
+    outputs: [
+      {
+        name: OreItemNames.RawQuartz,
+        rate: 120,
+      },
+    ],
+  },
+  {
+    name: "Sulfur (Coal)",
+    building: BuildingNames.Converter,
+    inputs: [
+      {
+        name: ElectronicItemNames.ReanimatedSAM,
+        rate: 10,
+      },
+      {
+        name: OreItemNames.Coal,
+        rate: 200,
+      },
+    ],
+    outputs: [
+      {
+        name: OreItemNames.Sulfur,
+        rate: 120,
+      },
+    ],
+  },
+  {
+    name: "Sulfur (Iron)",
+    building: BuildingNames.Converter,
+    inputs: [
+      {
+        name: ElectronicItemNames.ReanimatedSAM,
+        rate: 10,
+      },
+      {
+        name: OreItemNames.IronOre,
+        rate: 300,
+      },
+    ],
+    outputs: [
+      {
+        name: OreItemNames.Sulfur,
+        rate: 120,
+      },
+    ],
+  },
+  {
+    name: "Bauxite (Caterium)",
+    building: BuildingNames.Converter,
+    inputs: [
+      {
+        name: ElectronicItemNames.ReanimatedSAM,
+        rate: 10,
+      },
+      {
+        name: OreItemNames.CateriumOre,
+        rate: 150,
+      },
+    ],
+    outputs: [
+      {
+        name: OreItemNames.Bauxite,
+        rate: 120,
+      },
+    ],
+  },
+  {
+    name: "Bauxite (Copper)",
+    building: BuildingNames.Converter,
+    inputs: [
+      {
+        name: ElectronicItemNames.ReanimatedSAM,
+        rate: 10,
+      },
+      {
+        name: OreItemNames.CopperOre,
+        rate: 180,
+      },
+    ],
+    outputs: [
+      {
+        name: OreItemNames.Bauxite,
+        rate: 120,
+      },
+    ],
+  },
+  {
+    name: "Uranium Ore (Bauxite)",
+    building: BuildingNames.Converter,
+    inputs: [
+      {
+        name: ElectronicItemNames.ReanimatedSAM,
+        rate: 10,
+      },
+      {
+        name: OreItemNames.Bauxite,
+        rate: 480,
+      },
+    ],
+    outputs: [
+      {
+        name: OreItemNames.Uranium,
+        rate: 120,
+      }
+    ],
+  },
 ];
 
 export function getRecipesByOutput(o: ItemName) {
