ecd5142c473ab745a50a25f6435b7cf193e7abc3
IronBlood <aplshenyang@gmail.com>
2026-02-26 11:13:05 +0800

    all ore items

--- src/data/items.ts
+++ src/data/items.ts
@@ -16,6 +16,14 @@ export type OrePurity = typeof OrePurities[keyof typeof OrePurities];
 export const ItemNames = {
   Limestone: "Limestone",
   IronOre: "Iron Ore",
+  CopperOre: "Copper Ore",
+  CateriumOre: "Caterium Ore",
+  Coal: "Coal",
+  RawQuartz: "Raw Quartz",
+  Sulfur: "Sulfur",
+  Bauxite: "Bauxite",
+  SAM: "SAM",
+  Uranium: "Uranium",
 };
 
 export type ItemName = typeof ItemNames[keyof typeof ItemNames];
@@ -53,6 +61,14 @@ export const Items: Item[] = [
 const ITEM_IMAGE_MAP: Record<ItemName, string> = {
   [ItemNames.Limestone]: "Stone_256.png",
   [ItemNames.IronOre]: "IconDesc_iron_new_256.png",
+  [ItemNames.CopperOre]: "IconDesc_copper_new_256.png",
+  [ItemNames.CateriumOre]: "IconDesc_CateriumOre_256.png",
+  [ItemNames.Coal]: "IconDesc_CoalOre_256.png",
+  [ItemNames.RawQuartz]: "IconDesc_QuartzCrystal_256.png",
+  [ItemNames.Sulfur]: "Sulfur_256.png",
+  [ItemNames.Bauxite]: "IconDesc_Bauxite_256.png",
+  [ItemNames.SAM]: "IconDesc_SameOre_256.png",
+  [ItemNames.Uranium]: "IconDesc_UraniumOre_256.png",
 };
 
 export function getItemImageByName(name: ItemName) {
