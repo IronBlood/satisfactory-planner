e2f3b5493a4eb0720fee6e753f2df20d4f27a6bb
IronBlood <aplshenyang@gmail.com>
2026-03-04 23:06:34 +0800

    refactor: split implementations

--- src/nodes/BuildingNode.tsx
+++ src/nodes/BuildingNode.tsx
@@ -11,17 +11,42 @@ export type SupportedBuildings =
   | typeof BuildingNames.ResourceWellPressurizer
   ;
 
-export type BuildingNodeType = Node<{
-  name: SupportedBuildings;
+type BaseBuildingData = {
   count: number;
   isLocked: boolean;
-}, "building">;
+};
 
-export default function BuildingNode(props: NodeProps<BuildingNodeType>) {
+type AwesomeSinkData = BaseBuildingData & {
+  name: typeof BuildingNames.AwesomeSink;
+};
+
+type RWPData = BaseBuildingData & {
+  name: typeof BuildingNames.ResourceWellPressurizer;
+};
+
+type BuildingData = AwesomeSinkData | RWPData;
+
+type AwesomeSinkNodePropsType = NodeProps<Node<AwesomeSinkData, "building">>;
+export type BuildingNodeType = Node<BuildingData, "building">;
+type BuildingNodePropsType = NodeProps<BuildingNodeType>;
+
+function isAwesomeSinkNode(props: BuildingNodePropsType): props is AwesomeSinkNodePropsType {
+  return props.data.name === BuildingNames.AwesomeSink;
+}
+
+function AwesomeSinkNode(props: AwesomeSinkNodePropsType) {
   return (
     <div
     >
       {props.data.name} - {props.data.count} - {props.data.isLocked}
     </div>
   );
+}
+
+export default function BuildingNode(props: BuildingNodePropsType) {
+  if (isAwesomeSinkNode(props)) {
+    return AwesomeSinkNode(props);
+  }
+
+  throw new Error("TODO");
 }
