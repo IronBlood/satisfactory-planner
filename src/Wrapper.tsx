0e468c4bd35775649928029985e6f6918a882fc6
IronBlood <aplshenyang@gmail.com>
2026-03-06 16:51:20 +0800

    select, add, delete and rename a flow

--- src/Wrapper.tsx
+++ src/Wrapper.tsx
@@ -1,11 +1,24 @@
 import {
   useCallback,
+  useMemo,
   useRef,
+  useState,
 } from "react";
 import {
   ReactFlowProvider,
 } from "@xyflow/react";
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOption,
+  ListboxOptions,
+} from "@headlessui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlus,
+  faMinus,
+  faPencil,
+} from "@fortawesome/free-solid-svg-icons";
 import {
   faGithub,
 } from "@fortawesome/free-brands-svg-icons";
@@ -15,7 +28,8 @@ import App, {
 import type {
   MultiFlow,
 } from "./types";
-import { stripeData, useDataContext } from "./DataProvider";
+import { getDefaultFlow, stripeData, useDataContext } from "./DataProvider";
+import { ChevronDownIcon, CheckIcon } from "@heroicons/react/20/solid";
 
 function MenuButton({
   text,
@@ -48,10 +62,16 @@ function FooterLink({
 }
 
 function Wrapper() {
+  const [activeIdx, _setActiveIdx] = useState(0);
   const actionsRef = useRef<ActionsRef>({});
   const fileInputRef = useRef<HTMLInputElement | null>(null);
 
   const { data, setData } = useDataContext();
+
+  const list = useMemo(() => data.flows.map((flow, idx) => ({
+    id: idx,
+    name: flow.name,
+  })), [data]);
 
   const exportFlow = useCallback(() => {
     const stripedData = stripeData(data);
@@ -93,10 +113,111 @@ function Wrapper() {
     }
   }, []);
 
+  const setActiveIdx = useCallback((idx: number) => {
+    if (idx < 0) {
+      idx = 0;
+    }
+
+    if (idx > data.flows.length) {
+      idx = data.flows.length - 1;
+    }
+
+    _setActiveIdx(idx | 0);
+  }, [activeIdx, _setActiveIdx, data]);
+
+  const addFlow = useCallback(() => {
+    setData({
+      ...data,
+      flows: [
+        ...data.flows,
+        getDefaultFlow(),
+      ],
+    });
+  }, [data, setData]);
+
+  const deleteFlow = useCallback(() => {
+    if (data.flows.length <= 1) {
+      return;
+    }
+
+    setData({
+      ...data,
+      flows: [
+        ...data.flows.slice(0, activeIdx),
+        ...data.flows.slice(activeIdx + 1),
+      ],
+    });
+
+    setActiveIdx(0);
+  }, [data, activeIdx, setData, setActiveIdx]);
+
+  const renameFlow = useCallback(() => {
+    setData({
+      ...data,
+      flows: data.flows.map((flow, idx) => idx === activeIdx
+        ? { ...flow, name: "TODO" }
+        : flow
+      ),
+    });
+  }, [data, activeIdx]);
+
+  const selectedFlowName = useMemo(() => list[activeIdx], [list, activeIdx]);
+
   return (
     <div className="h-screen w-screen flex flex-col">
       <header className="h-16 border-b border-slate-800 px-4 flex items-center justify-between bg-slate-950 text-white text-xl">
         <div>Yet Another Satisfactory Planner</div>
+        <div className="w-46 gap-2 flex justify-between items-center">
+          <Listbox value={selectedFlowName} onChange={(v) => setActiveIdx(v.id)}>
+            <ListboxButton
+              className="relative rounded-lg bg-slate-800 text-left text-sm text-slate-300 py-1.5 px-2 data-focus:outline-2 flex items-center justify-between focus:not-data-focus:outline-none data-focus:-outline-offset-2"
+            >
+              {selectedFlowName.name}
+              <ChevronDownIcon
+                className="group pointer-events-none aboslute size-4 fill-white/60"
+                aria-hidden="true"
+              />
+            </ListboxButton>
+            <ListboxOptions
+              anchor="bottom"
+              transition
+              className="w-(--button-width) rounded-xl border border-slate-700 bg-slate-800 p-1 [--anchor-gap:--spacing(1)] focus:outline-none transition duration-100 ease-in data-leav:data-closed:opacity-0"
+            >
+              {list.map((item, idx) => (
+                <ListboxOption
+                  key={`${idx}-${item.name}`}
+                  value={item}
+                  className="group flex cursor-default items-center gap-2 rounded-lg px-1 py-1 select-none data-focus:bg-slate-700"
+                >
+                  <CheckIcon className="invisible size-4 fill-white group-data-selected:visible" />
+                  <div className="text-sm/6 text-slate-300">{item.name}</div>
+                </ListboxOption>
+              ))}
+            </ListboxOptions>
+          </Listbox>
+          <div
+            className="text-slate-300 text-sm hover:text-slate-100"
+          >
+            <FontAwesomeIcon
+              className="cursor-pointer"
+              icon={faPlus}
+              onClick={addFlow}
+            />
+            <FontAwesomeIcon
+              className={[
+                "",
+                data.flows.length === 1 ? "text-slate-600" : "cursor-pointer",
+              ].join(" ")}
+              icon={faMinus}
+              onClick={deleteFlow}
+            />
+            <FontAwesomeIcon
+              className="cursor-pointer"
+              icon={faPencil}
+              onClick={renameFlow}
+            />
+          </div>
+        </div>
         <div className="flex gap-2">
           <MenuButton
             onClick={() => exportFlow()}
