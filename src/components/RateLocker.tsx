4349293e87be4888b9b23edce183ceaa6d1cbfac
IronBlood <aplshenyang@gmail.com>
2026-03-14 15:52:05 +0800

    use fontawesome

--- src/components/RateLocker.tsx
+++ src/components/RateLocker.tsx
@@ -3,9 +3,12 @@ import {
   useReactFlow,
 } from "@xyflow/react";
 import {
-  LockOpenIcon,
-  LockClosedIcon,
-} from "@heroicons/react/24/outline";
+  FontAwesomeIcon,
+} from "@fortawesome/react-fontawesome";
+import {
+  faLock,
+  faLockOpen,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function RateLocker({
   nodeId,
@@ -24,14 +27,16 @@ export default function RateLocker({
   return (
     <button
       className={[
-        "h-4 w-4 p-0.5 bg-slate-800 rounded-full transition",
+        "h-4 w-4 p-0.5 bg-slate-800 rounded-full transition text-xs duration-200",
         isLocked
           ? "text-yellow-700 hover:text-yellow-500"
           : "text-slate-700 hover:text-slate-500"
       ].join(" ")}
       onClick={toggle}
     >
-      {isLocked ? <LockClosedIcon /> : <LockOpenIcon />}
+      <FontAwesomeIcon
+        icon={isLocked ? faLock : faLockOpen}
+      />
     </button>
   );
 }
