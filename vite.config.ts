875a1da0de5b79d2b5e8589c4774dc26bd9a3032
IronBlood <aplshenyang@gmail.com>
2026-03-16 13:59:35 +0800

    fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { fileURLToPath } from 'url'
 
+const repoName = process.env.VITE_REPO_NAME || '/';
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: repoName,
   plugins: [
     react(),
     tailwindcss(),
